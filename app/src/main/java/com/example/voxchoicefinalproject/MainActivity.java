0f6e7d24019f3b6a3d5d8b2bdab9802c8c742e5d
--get <95505981+ekamtaneja@users.noreply.github.com>
2024-03-06 23:00:33 -0800

    fixed mainActivity, added idea files.

--- app/src/main/java/com/example/voxchoicefinalproject/MainActivity.java
+++ app/src/main/java/com/example/voxchoicefinalproject/MainActivity.java
@@ -1,38 +1,14 @@
 package com.example.voxchoicefinalproject;
 
-import static com.example.voxchoicefinalproject.R.*;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    String username, password;
-
-    EditText usernameInput;
-    EditText passwordInput;
-    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_login);
-
-        usernameInput = (EditText) findViewById(R.id.usernameInput);
-        passwordInput = (EditText) findViewById(R.id.passwordInput);
-
-        loginButton = (Button) findViewById(id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener()[
-            @Override
-            public void onClick(View v) {
-
-            }
-
-        ]);
+        setContentView(R.layout.activity_main);
     }
 }
