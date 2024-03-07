9a3066f4478b6731fb0e71297df5abc3467858a7
--get <95505981+ekamtaneja@users.noreply.github.com>
2024-03-06 22:59:08 -0800

    Added title and text box hints for activity_login.xml

--- app/src/main/java/com/example/voxchoicefinalproject/MainActivity.java
+++ app/src/main/java/com/example/voxchoicefinalproject/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.voxchoicefinalproject;
 
+import static com.example.voxchoicefinalproject.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String username, password;
+
+    EditText usernameInput;
+    EditText passwordInput;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_login);
+
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        loginButton = (Button) findViewById(id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener()[
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        ]);
     }
 }
